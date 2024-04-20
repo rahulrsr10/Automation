5f896ffaba88548b3ad4eb364c367c32e79578be
rahulrsr10 <114585917+rahulrsr10@users.noreply.github.com>
2024-04-20 16:51:15 +0530

    screen shot methods added

--- src/test/java/automationdemopack/RegisterTest.java
+++ src/test/java/automationdemopack/RegisterTest.java
@@ -1,31 +1,43 @@
 package automationdemopack;
 
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import uiutilities.UiActionMethods;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.Test;
 
 public class RegisterTest extends BaseTest {
-	SoftAssert softassert;
-	UiActionMethods uiActions;
 	@Test
-	public void testRegiterFunctionality() throws InterruptedException {
+	public void testRegiterFunctionality() throws InterruptedException, IOException {
 		RegistrationDemoPage registerPage = new RegistrationDemoPage(driver);
 		registerPage.enterFirstName("Rahul");
 		registerPage.enterLastName("Rode");
 		registerPage.selectGender(driver, "Male");
 		registerPage.selectExperience(driver, 5);
+		//Convert web driver object to TakeScreenshot
+		TakesScreenshot scrShot =((TakesScreenshot)driver);
+		//Call getScreenshotAs method to create image file
+		File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+		//Move image file to new destination
+		File DestFile=new File("C:\\Users\\HP\\eclipse-workspace\\Automation\\test-output\\1.png");
+		//Copy file at destination
+		FileUtils.copyFile(SrcFile, DestFile);
+	    Files.deleteIfExists(Paths.get("C:\\Users\\HP\\eclipse-workspace\\Automation\\test-output\\1.png"));
 	}
-
-	@Test
-	public void testdropdown() throws InterruptedException {
-		RegistrationDemoPage registerPage = new RegistrationDemoPage(driver);
-		registerPage.selectvaluesfromdropdown(driver);
-	}
-	
-	@Test
-	public void exceptionHandling() {
-		RegistrationDemoPage registerPage = new RegistrationDemoPage(driver);
-		registerPage.exceptionHandling(driver);	
-	}
+//
+//	@Test
+//	public void testdropdown() throws InterruptedException {
+//		RegistrationDemoPage registerPage = new RegistrationDemoPage(driver);
+//		registerPage.selectvaluesfromdropdown(driver);
+//	}
+//	
+//	@Test
+//	public void exceptionHandling() {
+//		RegistrationDemoPage registerPage = new RegistrationDemoPage(driver);
+//		registerPage.exceptionHandling(driver);	
+//	}
 }
